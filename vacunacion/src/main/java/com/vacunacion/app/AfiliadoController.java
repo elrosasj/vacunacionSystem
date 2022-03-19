8957185435194a7186c84ec14217dd40f488a54a
elrosasj <eloy.rosas.j@gmail.com>
2022-03-18 22:18:14 -0600

    Se avanza en formulario de registro de afiliado

--- vacunacion/src/main/java/com/vacunacion/app/AfiliadoController.java
+++ vacunacion/src/main/java/com/vacunacion/app/AfiliadoController.java
@@ -1,8 +1,18 @@
 package com.vacunacion.app;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.vacunacion.dto.Afiliado;
@@ -11,12 +21,26 @@ import com.vacunacion.dto.Afiliado;
 @SessionAttributes(value = "afiliado")
 public class AfiliadoController {
 
+//	@InitBinder
+//	public void initBinder(WebDataBinder binder) {
+//		SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+//		formato.setLenient(false);
+//		binder.registerCustomEditor(Date.class, new CustomDateEditor(formato, false));
+//	}
 	
-	@RequestMapping (value = "/alta")
-	public String alta (Model model) {
+	@RequestMapping (value = "/mostrarAfiliado")
+	public String mostrarVista (Model model) {
 		Afiliado afiliado = new Afiliado();
 		model.addAttribute("afiliado", afiliado);
-		model.addAttribute("titulo", "Vacunacion integral");
-		return "index";
+		model.addAttribute("titulo", "Formulario de registro de afiliado");
+		return "formAfiliado";
 	}
+	
+	@RequestMapping (value = "/registrar" , method = RequestMethod.POST)
+	public String registrarAfiliado (@Valid Afiliado afiliado, BindingResult result, Model model) {
+		model.addAttribute("afiliado", afiliado);
+		model.addAttribute("titulo", "Formulario de registro de afiliado");
+		return "formAfiliado";
+	}
+		
 }
--- vacunacion/src/main/java/com/vacunacion/dto/Afiliado.java
+++ vacunacion/src/main/java/com/vacunacion/dto/Afiliado.java
@@ -2,76 +2,135 @@ package com.vacunacion.dto;
 
 import java.util.Date;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 public class Afiliado {
 	private Long idAfiliado;
+
+	@NotEmpty
 	private String nombre;
+	@NotEmpty
 	private String apellidoPaterno;
+	@NotEmpty
 	private String apellidoMaterno;
+	@NotEmpty
 	private String sexo;
+	@NotNull
+	@DateTimeFormat(pattern = "dd-MM-yyyy")
 	private Date fechaNacimiento;
+
+	@NotEmpty
 	private String curp;
-	private String institucionAsegura;
 	private String matriculaAsegurado;
-	private Direccion direccion;
+	private String codigoPostal;
 	
+	@NotEmpty
+	@Email
+	private String email;
+
+	@NotEmpty
+	private String telefonoFijo;
+	@NotEmpty
+	private String telefonoCelular;
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellidoPaterno() {
 		return apellidoPaterno;
 	}
+
 	public void setApellidoPaterno(String apellidoPaterno) {
 		this.apellidoPaterno = apellidoPaterno;
 	}
+
 	public String getApellidoMaterno() {
 		return apellidoMaterno;
 	}
+
 	public void setApellidoMaterno(String apellidoMaterno) {
 		this.apellidoMaterno = apellidoMaterno;
 	}
+
 	public String getSexo() {
 		return sexo;
 	}
+
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
+
 	public Date getFechaNacimiento() {
 		return fechaNacimiento;
 	}
+
 	public void setFechaNacimiento(Date fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
 	}
+
 	public String getCurp() {
 		return curp;
 	}
+
 	public void setCurp(String curp) {
 		this.curp = curp;
 	}
-	public String getInstitucionAsegura() {
-		return institucionAsegura;
-	}
-	public void setInstitucionAsegura(String institucionAsegura) {
-		this.institucionAsegura = institucionAsegura;
-	}
+
 	public String getMatriculaAsegurado() {
 		return matriculaAsegurado;
 	}
+
 	public void setMatriculaAsegurado(String matriculaAsegurado) {
 		this.matriculaAsegurado = matriculaAsegurado;
 	}
+
 	public Long getIdAfiliado() {
 		return idAfiliado;
 	}
+
 	public void setIdAfiliado(Long idAfiliado) {
 		this.idAfiliado = idAfiliado;
 	}
-	public Direccion getDireccion() {
-		return direccion;
+
+	public String getCodigoPostal() {
+		return codigoPostal;
 	}
-	public void setDireccion(Direccion direccion) {
-		this.direccion = direccion;
+
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefonoFijo() {
+		return telefonoFijo;
+	}
+
+	public void setTelefonoFijo(String telefonoFijo) {
+		this.telefonoFijo = telefonoFijo;
+	}
+
+	public String getTelefonoCelular() {
+		return telefonoCelular;
+	}
+
+	public void setTelefonoCelular(String telefonoCelular) {
+		this.telefonoCelular = telefonoCelular;
+	}
+
 }
